b8f4caa0dee6966534742fa5a1707f174a059911
Hendra Wijaya <hendrahuang92@gmail.com>
2015-04-26 18:31:50 +0700

    add css specificity and still checking matchedNodes in annotatedRules

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var SelectorMatching = function() {
   };
 
   var getRightMostSelector = function(selector) {
-    var selectorToken = /(?=\.)|(?=#)|(?=\[)|(?= )/,
+    var selectorToken = /(?=\.)|(?=#)|(?=\[)|(?= \w+)|\>|\+/,
       tokenizedSelector = selector.split(selectorToken)
     ;
 
@@ -223,6 +223,83 @@ while (stack.length > 0) {
   }
 }
 //console.log(selectableDOM);
+
+// Order Specificity
+var orderSpecificity = function(rules) {
+  var getSelectorScore = function(selector) {
+    var selectorToken = /(?=\.)|(?=#)|(?=\[)|(?= \w+)|\>|\+/,
+      tokenizedSelector = selector.split(selectorToken),
+      score = 0
+    ;
+
+    tokenizedSelector.forEach(function(token, index) {
+      if (token.trim()[0] == '#') {
+        score += 100;
+      } else if (token.trim()[0] == '.' || token[0] == '[') {
+        score += 10;
+      } else if (token.trim()[0].match(/^\w+/)) {
+        score += 1;
+      }
+    })
+
+    return score;
+  };
+
+  rules.forEach(function(rule, index) {
+    var selector = rule.selector,
+      scores = [],
+      orderedRules = [],
+      score = getSelectorScore(selector)
+    ;
+
+    scores.push(score);
+    orderedRules.push(rule);
+    if (scores.length > 1) {
+      for (var i = scores.length - 1; i > 0; i--) {
+        if (scores[i - 1] < scores[i]) {
+          // swap the score
+          var temp = scores[i - 1];
+          scores[i - 1] = scores[i];
+          scores[i] = temp;
+          // swap the selector
+          var temp = orderedRules[i - 1];
+          orderedRules[i - 1] = orderedRules[i];
+          orderedRules[i] = temp;
+        } else if (scores[i - 1] == score[i]) {
+          if (orderedRules[i - 1].orderPosition < orderedRules.orderPosition) {
+            // swap the selector
+            var temp = orderedRules[i - 1];
+            orderedRules[i - 1] = orderedRules[i];
+            orderedRules[i] = temp;
+          }
+        }
+      }
+    }
+
+    return orderedRules;
+  });
+};
+
+// Algorithm Effective Selector
+selectableDOM.forEach(function(dom) {
+  var getMatchingRules = function(dom) {
+    annotatedRules.forEach(function(rule) {
+      var matchedRules = [];
+
+      rule.matchedNodes.forEach(function(node) {
+        if (node.id == dom.id) {
+          matchedRules.push(rule);
+        }
+      });
+    });
+
+    return matchedRules;
+  };
+
+  var rules = getMatchingRules(dom);
+  console.log(dom.name);
+  console.log(rules);
+});
 
 //var result = css.stringify(ast);
 //console.log(ast.stylesheet.rules[0]);
